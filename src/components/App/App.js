bdc6a25e9f80ca540303e244d17800cab9827add
glebradnikov <glebradnikov@yandex.ru>
2023-07-19 04:47:49 +0300

    feat: prepare the necessary routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import { Main } from '../Main/Main';
+import { Movies } from '../Movies/Movies';
+import { SavedMovies } from '../SavedMovies/SavedMovies';
+import { Profile } from '../Profile/Profile';
+import { Login } from '../Login/Login';
+import { Register } from '../Register/Register';
+
 export const App = () => {
-  return <div className="app"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/saved-movies" element={<SavedMovies />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/signin" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
+    </Routes>
+  );
 };
